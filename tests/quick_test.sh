846dcb1cc95f807183cda8c5ced05cbd321f8686
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-03 17:23:09 -0400

    Don't hard-code owner, repo and version in quick_test.sh

--- tests/quick_test.sh
+++ tests/quick_test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # Usage: ./quick_test.sh
 
-version="v0.2.2"
-temp_dir="/tmp/${USER}/ntangle/quick"
+owner="OrgTangle"
+repo="ntangle"
+latest_release_url="$(curl -s "https://api.github.com/repos/${owner}/${repo}/releases/latest" | grep 'browser_download_url.*\.xz' | cut -d: -f2,3 | sed 's/[" ]//g')"
+archive_name="$(echo "${latest_release_url}" | rev | cut -d/ -f1 | rev)"
+temp_dir="/tmp/${USER}/${repo}/quick"
 test="tangle_no_yes"
 
 if [[ -d "${temp_dir}" ]]
@@ -15,17 +18,16 @@ cd "${temp_dir}" || exit
 ls -la
 
 echo ""
-
-echo "Downloading ntangle ${version} .."
-wget -nd https://github.com/OrgTangle/ntangle/releases/download/"${version}"/ntangle-"${version}".Linux_64bit_musl.tar.xz
-tar xf ntangle-"${version}".Linux_64bit_musl.tar.xz
+echo "Downloading ${latest_release_url} .."
+wget -nd "${latest_release_url}"
+tar xf "${archive_name}"
 
 echo ""
 echo "Downloading test ${test} .."
-wget -nd "https://raw.githubusercontent.com/OrgTangle/ntangle/master/tests/${test}/${test}.org"
+wget -nd "https://raw.githubusercontent.com/${owner}/${repo}/master/tests/${test}/${test}.org"
 
 echo ""
-cmd="./ntangle ${test}.org"
+cmd="./${repo} ${test}.org"
 echo "Running ${cmd} .."
 eval "${cmd}"
 
