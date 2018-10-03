7d283fdc639f5a1049be59e3df342acf5fd17147
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-03 13:39:29 -0400

    Re-organize the tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Time-stamp: <2018-10-03 13:05:04 kmodi>
+# Time-stamp: <2018-10-03 13:36:45 kmodi>
 
 set -euo pipefail # http://redsymbol.net/articles/unofficial-bash-strict-mode
 IFS=$'\n\t'
@@ -8,29 +8,31 @@ repo_root="$(git rev-parse --show-toplevel)"
 
 run_test () {
     bin="$1"
-    "${bin}" tests/test1.org
-    "${bin}" tests/tangle_no_yes.org
-    "${bin}" tests/src_blocks_with_extra_indentation.org
-    "${bin}" tests/multiple_src_blocks_tangled_to_same_file.org
-    "${bin}" tests/write-yourself-a-git.org
-    "${bin}" tests/shebang.org
-    "${bin}" tests/missing_arg_value.org || true
+    "${bin}" tests/test1/test1.org
+    "${bin}" tests/tangle_no_yes/tangle_no_yes.org
+    "${bin}" tests/src_blocks_with_extra_indentation/src_blocks_with_extra_indentation.org
+    "${bin}" tests/multiple_src_blocks_tangled_to_same_file/multiple_src_blocks_tangled_to_same_file.org
+    "${bin}" tests/wyag/write-yourself-a-git.org
+    "${bin}" tests/shebang/shebang.org
+    "${bin}" tests/missing_arg_value/missing_arg_value.org || true
 
     rm -rf ./tests/foo/bar/
-    "${bin}" tests/mkdirp_no.org || true
-    "${bin}" tests/mkdirp_yes.org
+    "${bin}" tests/mkdirp_no/mkdirp_no.org || true
+    "${bin}" tests/mkdirp_yes/mkdirp_yes.org
 
     # Test tangling an Org file in the same dir.
-    cd tests || exit
-    "../${bin}" tangle_mode.org
+    cd tests/tangle_mode || exit
+    "../../${bin}" tangle_mode.org
 
-    # "${bin}" tests/eless.org
+    # "${bin}" tests/eless/eless.org
 }
 
+# Regular build
 cd "${repo_root}"
 nimble build -d:release
 run_test "./ntangle"
 
+# musl build
 cd "${repo_root}"
 nim musl src/ntangle.nim
 run_test "./src/ntangle"
