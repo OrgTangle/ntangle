7b4d0d5e6d766fc778b64043ddeadf398c6be7db
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-07 10:59:36 -0400

    Add @dakra dmacs as test

---
+++ tests/dmacs/early-init.el
@@ -0,0 +1,3 @@
+;; [[file:~/stow/pub_dotfiles/emacs/dot-emacs.d/misc/ntangle/tests/dmacs/init.org::*Package%20Settings][Package Settings:1]]
+(setq package-enable-at-startup nil)
+;; Package Settings:1 ends here
