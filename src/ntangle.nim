41a71edf770f2dd3ad5294bb7a449a00c3863617
Vindaar <basti90@gmail.com>
2022-03-03 11:10:22 +0100

    allow value-less keys, change `LangAndArgs` tuple
    Implements support for keys without a value, as for example in:
    
    ```org
    φ = var('φ')
    ```
    
    Implemented by changing the data structure `LangAndArgs` to properly
    differentiate between keys and values without any string manipulation.

--- src/ntangle.nim
+++ src/ntangle.nim
@@ -52,7 +52,7 @@ type
   LangAndArgs = tuple
     argType: HeaderArgType
     lang: string
-    args: seq[string]
+    args: seq[tuple[key, val: string]] # args are key / value pairs.
   LevelLangIndex = tuple
     orgLevel: Natural
     lang: string
@@ -144,9 +144,7 @@ proc parseTangleHeaderProperties(file: string, lnum: int, haObj: LangAndArgs) =
 
   for hdrArg in haObj.args:
     let
-      hdrArgParts = hdrArg.strip.split(" ", maxsplit=1)
-      argkey = hdrArgParts[0]
-      argval = hdrArgParts[1]
+      (argKey, argVal) = hdrArg
     dbg "argkey={argkey}, argval={argval}, onBeginSrc={haObj.argType == haBeginSrc}, outfile={outfile}"
     case argkey
     of "tangle":
@@ -354,8 +352,8 @@ proc getHeaderArgs(file: string, line: string, lnum: int): LangAndArgs =
     spaceSepParts = line.strip.split(" ").filterIt(it != "")
   var
     haType: HeaderArgType = haNone
-    headerArgsRaw: seq[string] = @[]
-    headerArgs: seq[string] = @[]
+    headerArgsRaw: seq[string]
+    headerArgs: seq[tuple[key, val: string]]
     headerArgPair: string
     lang: string
   dbg "spaceSepParts: {spaceSepParts}", dvHigh
@@ -400,20 +398,22 @@ proc getHeaderArgs(file: string, line: string, lnum: int): LangAndArgs =
     if startHeaderArgs >= 2:
       headerArgsRaw = spaceSepParts[startHeaderArgs .. spaceSepParts.high]
   if haType != haNone:
-    #echo headerArgsRaw
+    var lastKey = false
+    var keyVal: tuple[key, val: string]
     for i, h in headerArgsRaw:
-      if h.len >= 2 and h[0] == ':':
-        if i == headerArgsRaw.high:
-          raise newException(OrgError, fmt("The header args are ending with a ':key' which is not valid. Found {headerArgsRaw}"))
-        if headerArgPair != "":
-          headerArgs.add(headerArgPair)
-        headerArgPair = h[1 .. h.high]
-        #echo fmt"{i} - {headerArgPair}"
+      if h.len >= 2 and h[0] == ':': # this is a key
+        if lastKey: # if last was already a key, add `keyVal` to res
+          headerArgs.add keyVal
+          keyVal = (key: "", val: "")
+        keyVal.key = h[1 .. h.high]
+        lastKey = true
       else:
-        headerArgPair = headerArgPair & " " & h.strip(chars = {'"'})
-        #echo fmt"{i} - {headerArgPair}"
-        if i == headerArgsRaw.high:
-          headerArgs.add(headerArgPair)
+        keyVal.val = h
+        headerArgs.add keyVal
+        keyVal = (key: "", val: "")
+        lastKey = false
+    if keyVal.key.len > 0 or keyVal.val.len > 0: # add current `keyVal` if anything
+      headerArgs.add keyVal
   return (haType, lang, headerArgs)
 
 proc parseLine(file: string, line: string, lnum: int) =
